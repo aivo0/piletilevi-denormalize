a40ec0377f010fa2cbf80bf20ef614538ecfcbc3
Aivo Olev <aivo.olev@gmail.com>
2023-05-10 13:26:33 +0300

    Add server

--- json_to_cal.ts
+++ json_to_cal.ts
@@ -1,21 +1,27 @@
-export {}
+export { }
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-const res = await fetch("http://www.piletilevi.ee/api/?preset=kultrakvere&language=est");
-const body = await res.json();
-const categories = body.responseData.category;
-const shows = body.responseData.show;
-const events = body.responseData.concert;
-const promoters = body.responseData.promoter;
-const venues = body.responseData.venue;
-const results = [];
-for (const e in events) {
-    const event = events[e]
-    event.venue=venues.find(v => v.id === events[e].venueId)
-    event.show=shows.find(s => s.id === events[e].showId)
-    event.show.categories.map(id => {
-        return categories.find(c => c.id === id)
-    })
-    event.promoter=promoters.find(p => p.id === events[e].promoterId)
-    results.push(event)
-}
-await Deno.writeTextFile("./piletilevi.json", JSON.stringify({events:results}));
+serve(async (req: Request) => {
+    const res = await fetch("http://www.piletilevi.ee/api/?preset=kultrakvere&language=est");
+    const body = await res.json();
+    const categories = body.responseData.category;
+    const shows = body.responseData.show;
+    const events = body.responseData.concert;
+    const promoters = body.responseData.promoter;
+    const venues = body.responseData.venue;
+    const results = [];
+    for (const e in events) {
+        const event = events[e]
+        event.venue = venues.find(v => v.id === events[e].venueId)
+        event.show = shows.find(s => s.id === events[e].showId)
+        event.show.categories.map(id => {
+            return categories.find(c => c.id === id)
+        })
+        event.promoter = promoters.find(p => p.id === events[e].promoterId)
+        results.push(event)
+    }
+    // await Deno.writeTextFile("./piletilevi.json", JSON.stringify({events:results}));
+
+
+    return new Response(JSON.stringify({events:results}));
+});
